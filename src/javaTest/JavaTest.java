82111536cf298b7388003c831812b3f1a7705f04
peterfromearth <peterstumm@gmx.de>
2017-12-28 23:07:40 +0100

    add test

---
+++ src/javaTest/JavaTest.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package javaTest;
+
+/**
+ * @author peter
+ *
+ */
+public class JavaTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+	}
+
+}
