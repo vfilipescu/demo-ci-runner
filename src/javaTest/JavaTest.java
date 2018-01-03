ae37888be259779994b3fac835d333f461253c7a
peterfromearth <peterstumm@gmx.de>
2018-01-03 20:27:12 +0100

    add library

--- src/javaTest/JavaTest.java
+++ src/javaTest/JavaTest.java
@@ -2,6 +2,7 @@
  * 
  */
 package javaTest;
+import org.joda.time.LocalTime;
 
 /**
  * @author peter
@@ -9,12 +10,27 @@ package javaTest;
  */
 public class JavaTest {
 
+	static int a = 0;
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
+		
+		int a = 1;
+		System.out.println(JavaTest.a);
+		System.out.println(a);
+		
+		JavaTest jt = new JavaTest();
+		jt.run();
+	}
+	
+	public void run() {
+		System.out.println(JavaTest.a);
+
+		LocalTime currentTime = new LocalTime();
+		System.out.println("The current local time is: " + currentTime);
 	}
 
 }
