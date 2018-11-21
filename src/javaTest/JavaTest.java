66789147802d7c7b99cd794165a8dd7021ea8040
Vladimir <vfilipescu@yahoo.com>
2018-11-21 21:36:38 +0200

    vf commit test no.1

--- src/javaTest/JavaTest.java
+++ src/javaTest/JavaTest.java
@@ -4,30 +4,26 @@
 package javaTest;
 import org.joda.time.LocalTime;
 
-/**
- * @author peter
- *
- */
 public class JavaTest {
 
-	static int a = 0;
+//	static int a = 0;
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World!");
+		System.out.println("Servus Cluj, te iubesc!");
 		
-		int a = 1;
-		System.out.println(JavaTest.a);
-		System.out.println(a);
-		
+//		int a = 1;
+//		System.out.println(JavaTest.a);
+//		System.out.println(a);
+//
 		JavaTest jt = new JavaTest();
 		jt.run();
 	}
 	
 	public void run() {
-		System.out.println(JavaTest.a);
+//		System.out.println(JavaTest.a);
 
 		LocalTime currentTime = new LocalTime();
 		System.out.println("The current local time is: " + currentTime);
