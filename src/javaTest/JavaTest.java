2ed2ca87e245d7250b51decf87cee273278662a0
Vladimir <vfilipescu@yahoo.com>
2018-11-21 22:06:00 +0200

    vf commit test no.3

--- src/javaTest/JavaTest.java
+++ src/javaTest/JavaTest.java
@@ -8,7 +8,7 @@ public class JavaTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Servus Cluj, te iubesc!");
+		System.out.println("Servus Cluj, I love you!");
 
 		JavaTest jt = new JavaTest();
 		jt.run();
